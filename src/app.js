75e02afd4886f25d9c6115c82f0718fc12c677a9
JBritton2343 <jbritton854@gmail.com>
2022-08-15 19:43:32 -0500

    finished project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,49 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  function getCardValue(cardValue=0){
+  
+  switch(cardValue){
+    case 1: return "A";
+    break;
+    case 2: return 2;
+    break;
+    case 3: return 3;
+    break;
+    case 4: return 4;
+    break;
+    case 5: return 5;
+    break;
+    case 6: return 6;
+    break;
+    case 7: return 7;
+    break;
+    case 8: return 8;
+    break;
+    case 9: return 9;
+    break;
+    case 10: return 10;
+    break;
+    case 11: return "J";
+    break;
+    case 12: return "Q";
+    break;
+    case 13: return "K";
+    break;
+    default: return null;
+  }}
+  let suitsArr=["♦", "♥", "♠", "♣"];
+  let randVal=(Math.floor(Math.random()*13)+1);
+  let suit=suitsArr[Math.floor(Math.random()*3)]
+  document.body.innerHTML=`<div class="card">
+  <div class="topSuit ${suit=="♦"||suit=="♥"?"red":""}">${suit}</div>
+  <div class="cardValue">${getCardValue(randVal)}</div>
+  <div class="bottomSuit ${suit=="♦"||suit=="♥"?"red":""}">${suit}</div>
+  </div>`
 };
+
+
